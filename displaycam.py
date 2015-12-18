faec5e8cb2482994b6bd93e96ebb67c076b2b381
Michael Harig <software@michaelharig.de>
2015-12-18 19:09:30 -0400

    Changed to pygame.camera; using different screen  & cam resolutions work.

--- displaycam.py
+++ displaycam.py
@@ -1,43 +1,55 @@
 # Captures and displays video images
-
+from __future__ import print_function
 
 ###################################################################
-# TODO: use the pygame.camera module instead of VideoCapture!!!!!!!
+# TODO:
+#       - use max framerate instead of configured one (remove clock)!
+#       - make ESC & CTRL-C exit the program
 ###################################################################
+
+import os.path as op
 
 import pygame
-from VideoCapture import Device
+import pygame.camera as camera
 
 
+def printVideoDevices():
+    '''Prints a list of available video devices'''
+    camera.init()
+    print(camera.list_cameras())
 
-def initCam(_devnum=0, _res=(640, 480)):
+
+def initCam(_camera='/dev/video0', _res=(640, 480)):
     '''Wants the index of the wanted cam. Returns the device object and the resolution (w,h)'''
-    cam = Device(devnum=_devnum)
+
+    camera.init()
+    cam = camera.Camera(_camera, _res)
 
     if cam is None:
         raise Exception('Cannot connect to camera. Maybe in use by other program?')
 
-    try:
-        cam.setResolution(*_res)
-    except Exception:
-        print 'Warning: cannot set resolution.'
+    cam.start()
 
-    # capture 1 frame to determin resolution
-    img = cam.getImage()
-    print 'Camera "' + cam.getDisplayName() + '" resolution:', str(img.size)
-    return cam, img.size
+    return cam
 
 
 
 def initPygame(_windowSize=(640, 480)):
     pygame.init()
-    screen = pygame.display.set_mode(_windowSize)
+
+    # get display resolution and adjust screen size
+    vidinfo = pygame.display.Info()
+    w = min(vidinfo.current_w, _windowSize[0])
+    h = min(vidinfo.current_h, _windowSize[1])
+
+    screen = pygame.display.set_mode((w, h), 0)
+    snapshot = pygame.surface.Surface((w, h), 0, screen)
     clock = pygame.time.Clock()
-    return screen, clock
+    return screen, snapshot, clock
 
 
 
-def loop(_cam, _fps, _screen, _clock):
+def loop(_cam, _fps, _screen, _snapshot, _clock):
     done = False
     imgTicker = 1
     while not done:
@@ -46,36 +58,40 @@ def loop(_cam, _fps, _screen, _clock):
             if event.type == pygame.QUIT:
                 done = True
             elif event.type == pygame.MOUSEBUTTONUP:    # mouse click
+                img = _cam.get_image()     # use if cam.query_image(): ....
+
                 snapName = 'snapshot_' + str(imgTicker) + '.png'
-                _cam.saveSnapshot(snapName)
-                print 'Saved snapshot as', snapName
+                while op.exists(snapName):
+                    imgTicker += 1
+                    snapName = 'snapshot_' + str(imgTicker) + '.png'
+                pygame.image.save(img, snapName)
+                print('Saved snapshot as', snapName)
                 imgTicker += 1
 
-        img = _cam.getBuffer()  # returns tuple (raw data string, width, height). undocumented.
-        size = img[-2:]
-        img = img[0]
+        if _cam.query_image():
+            img = _cam.get_image()
+            _snapshot = pygame.transform.scale(img, (_screen.get_width(), _screen.get_height()))
+            _screen.blit(_snapshot, (0,0))
+            pygame.display.flip()
 
-        #screen.blit(pygame.image.frombuffer(img, size, 'RGB'), (0, 0)) # upside down
-        screen.blit(pygame.image.fromstring(img, size, 'RGB', True), (0, 0)) # flip
-        pygame.display.flip()
         _clock.tick(_fps)
 
 
 
 def usage():
-    print '''usage: displaycam [-h] [devidx] [width] [height]
+    print('''usage: displaycam [-h] [-l] [camera device] [width] [height]
     -h              prints this message and exits
-    devidx          index of the wanted cam
+    -l              lists avaiable device names and exits
+    camera device   name of the wanted cam
     width           video resolution width
     height          video resolution height
     If you provide the video width you must provide the height, too.
-    Setting the video resolution may not work.'''
+    Setting the video resolution may not work.''')
 
 
 if __name__ == '__main__':
     import sys
 
-    devnum = None
     W = None
     H = None
 
@@ -83,13 +99,9 @@ if __name__ == '__main__':
         if sys.argv[1] == '-h':
             usage()
             exit()
-        else:
-            try:
-                devnum = int(sys.argv[1])
-            except Exception, e:
-                usage()
-                print str(e)
-                exit()
+        if sys.argv[1] == '-l':
+            printVideoDevices()
+            exit()
         if len(sys.argv) == 4:
             W = int(sys.argv[2])
             H = int(sys.argv[3])
@@ -97,14 +109,21 @@ if __name__ == '__main__':
             usage()
             exit()
 
-    if devnum:
         if W:
-            cam, res = initCam(devnum, (W, H))
+            cam = initCam(sys.argv[1], (W, H))
         else:
-            cam, res = initCam(devnum)
+            cam = initCam(sys.argv[1])
+            W = 640
+            H = 480
     else:
-        cam, res = initCam()
+        cam = initCam()
+        W = 640
+        H = 480
+
     fps = 30
-    screen, clock = initPygame(res)
-    loop(cam, fps, screen, clock)
+
+    screen, snapshot, clock = initPygame(cam.get_size())
+    print('Using camera resolution', cam.get_size())
+    print('Using display size (%d, %d)'%(screen.get_width(), screen.get_height()))
+    loop(cam, fps, screen, snapshot, clock)
     pygame.quit()
